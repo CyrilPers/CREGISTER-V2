8877ccba47ecbd039355ff2e1427ec9c7c590412
Cyril <cyril.personne@outlook.fr>
2023-08-12 11:56:53 +0200

    ♻️ F03: LOGIN > LoginForm > TextInput to reusable folder

--- src/component/reusable-ui/TextInput.jsx
+++ src/component/reusable-ui/TextInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { styled } from 'styled-components';
-import { theme } from '../../../theme';
+import { theme } from '../../theme';
 
 export default function TextInput({value, onChange, Icon, className, ...extraProps}) {
   return (
