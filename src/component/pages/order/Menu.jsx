6570a7d3d729d35fa660f70573de2ba666acc61b
Cyril <cyril.personne@outlook.fr>
2023-08-14 15:15:58 +0200

    💄 F05: OrderPage > Product Style

--- src/component/pages/order/Menu.jsx
+++ src/component/pages/order/Menu.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { styled } from 'styled-components';
 import { fakeMenu2 } from '../../../fakeData/fakeMenu';
 import { theme } from '../../../theme';
+import ProductCard from './ProductCard';
 
 
 export default function Menu() {
@@ -10,29 +11,21 @@ export default function Menu() {
 
   return (
     <MenuStyled className='menu'>
-        {products.map((produit, index)=>{
-          return <div key={index} className='produit'>{produit.title}</div>
-        })}
-    </MenuStyled>
-  )
-}
+      {products.map((product, index) => {
+        return (
+          <ProductCard {...product} index={index} />
+          )
+          })}
+      </MenuStyled>
+    )
+  }
 
 const MenuStyled = styled.div`
-  background-color: blue;
+  background: ${theme.colors.background_white};
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-  background: ${theme.colors.background_white};
-
-
-
-  .produit {
-    width: 240px;
-    height: 330px;
-    box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
-    border-radius: ${theme.borderRadius.extraRound}
-  }
-`;
+    `;
--- src/component/pages/order/ProductCard.jsx
+++ src/component/pages/order/ProductCard.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import { styled } from 'styled-components';
+import { theme } from '../../../theme';
 
-export default function ProductCard() {
+export default function ProductCard({title, price, imageSource, index}) {
   return (
-    <div>ProductCard</div>
+    <ProductCardStyled key={index} className='product'>
+    <div className='image'>
+      <img src={imageSource} alt={title} />
+    </div>
+      <div className='description'>
+        <div className='title'>{title}</div>
+        <div className='price'>{price}</div>
+        <button className='add-to-cart'>Ajouter</button>
+      </div>
+    </ProductCardStyled>
   )
 }
+
+const ProductCardStyled = styled.div`
+  width: 240px;
+  height: 330px;
+  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+  border-radius: ${theme.borderRadius.extraRound};
+  
+  .title {
+
+  }
+
+  .image {
+    border: 1px solid fuchsia;
+    width: 100px;
+    height: auto;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  
+  }   
+
+`;
