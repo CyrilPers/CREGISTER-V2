19b943359be1d654f091c05dbda2f945d4b24475
Cyril <cyril.personne@outlook.fr>
2023-08-15 13:17:24 +0200

    💄 F06 : Navbar > Admin Mode Toast

--- src/component/navbar/NavbarUser.jsx
+++ src/component/navbar/NavbarUser.jsx
@@ -2,16 +2,43 @@ import React, { useState } from 'react'
 import { styled } from 'styled-components';
 import Profil from './Profile';
 import ToggleButton from '../reusable-ui/ToggleButton';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { theme } from '../../theme';
+
 
 
 export default function NavbarUser({username}) {
+
+  const [isModeAdmin, setIsModeAdmin] = useState(false)
+  
+  const notifyAdminMode = () => {
+    if (!isModeAdmin) {
+      toast.info("Mode admin activé", {
+        // icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    }
+    setIsModeAdmin(!isModeAdmin)
+  }
+
+  
   return (
     <NavbarUserStyled> 
-        <ToggleButton  
+        <ToggleButton
           labelIfChecked='DÉSACTIVER LE MODE ADMIN' 
           labelIfUnchecked='ACTIVER LE MODE ADMIN'
+          onToggle={notifyAdminMode}
         />
         <Profil username={username} />
+        <ToastContainer className="toaster" bodyClassName="body-toast" />
     </NavbarUserStyled>
   )
 }
@@ -20,4 +47,23 @@ const NavbarUserStyled = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
+  
 `;
