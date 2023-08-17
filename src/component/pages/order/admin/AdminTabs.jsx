43d246321b3786968d0b2ea27c80ae1c7977f8f4
Cyril <cyril.personne@outlook.fr>
2023-08-17 15:45:14 +0200

    💄  F07 : ADMIN >Add Collapsing AdminPanel

--- src/component/pages/order/admin/AdminTabs.jsx
+++ src/component/pages/order/admin/AdminTabs.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { styled } from 'styled-components';
 import Tab from '../../../reusable-ui/Tab';
 import {FiChevronDown, FiChevronUp} from 'react-icons/fi'
+import { theme } from '../../../../theme';
 
 
-export default function AdminTabs() {
+export default function AdminTabs({isCollapsed, setIsCollapsed}) {
+
+  const handleClick = () => { 
+    setIsCollapsed(!isCollapsed)
+   }
+
   return (
     <AdminTabsStyled>
-      <Tab Icon={<FiChevronDown />} />
+      <Tab Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown /> } 
+      onClick={handleClick} 
+      className={isCollapsed ? "is-active" : ""}
+      />
     </AdminTabsStyled>
   )
 }
@@ -16,4 +25,9 @@ const AdminTabsStyled = styled.div`
   display: flex;
   padding: 0 20px;
 
+  .is-active {
+    background: ${theme.colors.background_dark};
+    color: ${theme.colors.white};
+    border-color: ${theme.colors.white};
+  }
 `;
