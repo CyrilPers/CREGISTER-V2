172cffd08b73ca4c3593b0ca31dd0f1c2111ced4
Cyril <cyril.personne@outlook.fr>
2023-08-17 20:49:37 +0200

    ♻️ F07 : ADMIN > AdminTabs > Refacto

--- src/component/pages/order/admin/AdminTabs.jsx
+++ src/component/pages/order/admin/AdminTabs.jsx
@@ -19,21 +19,23 @@ export default function AdminTabs() {
     setIsEditSelected
   } = useContext(AdminContext)
 
-  const selectEditTab = () => { 
-    setIsCollapsed(false)
-    setIsEditSelected(true)
-    setIsAddSelected(false)
-   }
-
-   const selectAddTab = () => { 
-    setIsCollapsed(false)
-    setIsAddSelected(true)
-    setIsEditSelected(false)
-   }
 
   const handleClick = () => { 
     setIsCollapsed(!isCollapsed)
    }
+
+  const selectTab = (tabSelected) => {
+    setIsCollapsed(false)
+    
+    if (tabSelected === "add"){
+      setIsAddSelected(true)
+      setIsEditSelected(false)
+    } 
+    if (tabSelected === "edit"){
+      setIsEditSelected(true)
+      setIsAddSelected(false)
+    }
+  }
 
   return (
     <AdminTabsStyled>
@@ -46,13 +48,13 @@ export default function AdminTabs() {
       <Tab 
       label="Ajouter un produit"
       Icon={<AiOutlinePlus /> } 
-      onClick={selectAddTab} 
+      onClick={() => selectTab("add")} 
       className={isAddSelected ? "is-active" : ""}
       />
       <Tab 
       label="Modifier un produit"
       Icon={<MdModeEditOutline /> } 
-      onClick={selectEditTab} 
+      onClick={() => selectTab("edit")} 
       className={isEditSelected ? "is-active" : ""}
       />
     </AdminTabsStyled>
