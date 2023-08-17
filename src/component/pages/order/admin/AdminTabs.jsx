02bc8f43fec804a066724e97b65e8fe291b14d04
Cyril <cyril.personne@outlook.fr>
2023-08-17 14:23:59 +0200

    💄 F07: ADMIN > Abs > Create Reusable Tab

--- src/component/pages/order/admin/AdminTabs.jsx
+++ src/component/pages/order/admin/AdminTabs.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { styled } from 'styled-components';
+import Tab from '../../../reusable-ui/Tab';
+import {FiChevronDown, FiChevronUp} from 'react-icons/fi'
+
 
 export default function AdminTabs() {
   return (
     <AdminTabsStyled>
-      AdminTabs
+      <Tab Icon={<FiChevronDown />} />
     </AdminTabsStyled>
   )
 }
 
 const AdminTabsStyled = styled.div`
-  
+  display: flex;
+  padding: 0 20px;
+
 `;
