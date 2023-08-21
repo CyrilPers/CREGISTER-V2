c22eb0c5f8bbc1d5cb0ac6df867677e647cf9234
Cyril <cyril.personne@outlook.fr>
2023-08-21 10:41:54 +0200

    💄 F08/ ADMIN > AddForm > Add preview image style

--- src/component/pages/order/admin/admin-panel/AddForm.jsx
+++ src/component/pages/order/admin/admin-panel/AddForm.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import { styled } from 'styled-components';
 import AdminContext from '../../../../../context/AdminContext';
 import { FiCheck } from 'react-icons/fi'
+import { theme } from '../../../../../theme';
 
 const EMPTY_PRODUCT = {
     id:"",
@@ -45,7 +46,11 @@ export default function AddForm() {
   return (
     <AddFormStyled  onSubmit={handleSubmit}>
             <div className='image-preview' >
-                {newProduct.imageSource ? (<img src={newProduct.imageSource} alt={newProduct.title} /> ) : (<div>Aucune image</div> )}
+                {newProduct.imageSource ? (
+                <img src={newProduct.imageSource} alt={newProduct.title} /> 
+                ) : (
+                <div className='empty-img'>Aucune image</div> 
+                )}
             </div>       
             <div className='input-fields'>
             <input name="title" value={newProduct.title} onChange={handleChange} type="text" placeholder='Nom du produit' />
@@ -69,6 +74,8 @@ const AddFormStyled = styled.form`
     display: grid;
     grid-template-columns: 1fr 3fr;
     grid-template-rows: repeat(4, 1fr);
+    column-gap: 8px;
+    row-gap: 20px;
     height: 100%;
     width: 70%;
 
@@ -78,13 +85,25 @@ const AddFormStyled = styled.form`
         justify-content: center;
         align-items: center;
 
-
         img{
             width: 100%;
             height: 100%;
             object-fit: contain;
             object-position: center;
         }
+
+        .empty-img {
+            height: 100%;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid ${theme.colors.greyLight};
+            line-height: 1.5;
+            color: ${theme.colors.greySemiDark};
+            border-radius: ${theme.borderRadius.round};
+        }
+
     }
     .input-fields {
         grid-area: 1 / 2 / 4 / 5;
