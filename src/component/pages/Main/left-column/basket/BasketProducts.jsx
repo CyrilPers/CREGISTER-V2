b2b2da4f5372b3d941c4e13480799e5a4c247e11
Cyril <cyril.personne@outlook.fr>
2023-08-29 19:13:12 +0200

    💄 F14: BASKET > Transition effect on basketProducts

--- src/component/pages/Main/left-column/basket/BasketProducts.jsx
+++ src/component/pages/Main/left-column/basket/BasketProducts.jsx
@@ -4,6 +4,9 @@ import { theme } from '../../../../../theme';
 import BasketCard from './BasketCard';
 import { IMAGE_COMING_SOON } from '../../../../../enum/product.jsx'
 import AdminContext from '../../../../../context/AdminContext';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+
+
 
 export default function BasketProducts() {
 
@@ -18,18 +21,26 @@ export default function BasketProducts() {
     }
 
     return (
-        <BasketProductsStyled>
+        <TransitionGroup component={BasketProductsStyled}>
             {basket.map((basketProduct) =>
-                <div className='basket-card' key={basketProduct.id} >
-                    <BasketCard
-                        {...basketProduct}
-                        imageSource={basketProduct.imageSource ? basketProduct.imageSource : IMAGE_COMING_SOON}
-                        onDelete={() => handleOnDelete(basketProduct.id, username)}
-                    />
-                </div>
-            )
-            }
-        </BasketProductsStyled >
+                <CSSTransition
+                    appear={true}
+                    classNames={"card-transition"}
+                    key={basketProduct.id}
+                    timeout={500} // {{ enter: 500, exit: 500 }}
+                >
+                    <div className='basket-card'  >
+                        <BasketCard
+                            {...basketProduct}
+                            imageSource={basketProduct.imageSource ? basketProduct.imageSource : IMAGE_COMING_SOON}
+                            onDelete={() => handleOnDelete(basketProduct.id, username)}
+                            className={"card"}
+                        />
+                    </div>
+                </CSSTransition>
+            )}
+        </TransitionGroup>
+
     )
 }
 
@@ -42,6 +53,51 @@ const BasketProductsStyled = styled.div`
     overflow-y: scroll;
     scrollbar-color: transparent transparent;
     scrollbar-width: thin;
+    
+    .card-transition-appear {
+        .card{
+        transform: translateX(100px);
+        opacity: 0%;
+
+        }
+    }
+    .card-transition-appear-active {
+        .card{
+            transition: 0.5s;
+            transform: translateX(0px);
+            opacity: 100%;
+        }
+    }
+    .card-transition-enter {
+        .card{
+        transform: translateX(100px);
+        opacity: 0%;
+
+        }
+    }
+    .card-transition-enter-active {
+        .card{
+            transition: 0.5s;
+            transform: translateX(0px);
+            opacity: 100%;
+        }
+    }
+
+    .card-transition-exit {
+        .card{
+            transform: translateX(0px);
+            opacity: 100%;
+        }
+    }
+    .card-transition-exit-active {
+        .card{
+            transition: 0.5s;
+            transform: translateX(-100px);
+            /* transform: scale(0.1); */
+            opacity: 0%;
+        }
+    }
+
 
     &:hover {
         scrollbar-color: initial;
