2a9400034ffc634f0629a712724cb5690ced8f64
Cyril <cyril.personne@outlook.fr>
2023-08-30 15:14:58 +0200

    💄 F14: MENU > Product > Animation on add&delete

--- src/theme/animations.jsx
+++ src/theme/animations.jsx
@@ -64,10 +64,10 @@ export const adminAnimation = css`
 .admin-appear {
     opacity:0.1;
     transform: translateY(100%);
-&.admin-appear-active {
-    opacity:1;
-    transform: translateY(0);
-    transition: all ${theme.animation.speed.slow};
+    &.admin-appear-active {
+        opacity:1;
+        transform: translateY(0);
+        transition: all ${theme.animation.speed.slow};
     }
 }
 `
@@ -76,9 +76,9 @@ export const casinoAnimation = css`
 .count-animated-enter {
     transform: translateY(100%);
 
-&.count-animated-active {
-    transition: ${theme.animation.speed.slow};  
-    transform: translateY(0%);            
+    &.count-animated-active {
+        transition: ${theme.animation.speed.slow};  
+        transform: translateY(0%);            
     }
 }
 
@@ -88,11 +88,31 @@ export const casinoAnimation = css`
     right: 0;
     bottom: 0;
 
-&.count-animated-exit-active {
-    transition: ${theme.animation.speed.slow};
-    transform: translateY(-100%);
+    &.count-animated-exit-active {
+        transition: ${theme.animation.speed.slow};
+        transform: translateY(-100%);
     }
 }
 
 `
 
+export const menuAnimation = css`
+    .animation-card-enter {
+        opacity: 0.1;
+        transform: translateX(-100%);
+        &.animation-card-enter-active {
+            opacity: 1;
+            transform: translateX(0);
+            transition: all ${theme.animation.speed.medium};
+        }
+    }
+    .animation-card-exit {
+        opacity: 1;
+        transform: translateX(0);
+        &.animation-card-exit-active {
+            opacity: 0.1;
+            transform: translateX(-100%);
+            transform: all ${theme.animation.speed.medium};
+        }
+    } 
+`
