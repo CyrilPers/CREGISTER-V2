f0333736d529262c26fb60a0b5edef4926628354
Cyril <cyril.personne@outlook.fr>
2023-08-31 14:00:41 +0200

    ♻️ F15: ADMIN > Form > Refactor by creating FormInputs

--- src/component/pages/order/admin/admin-panel/Form.jsx
+++ src/component/pages/order/admin/admin-panel/Form.jsx
@@ -4,6 +4,7 @@ import ImagePreview from './ImagePreview';
 import TextInput from '../../../../reusable-ui/TextInput';
 import { getInputSelectConfig, getInputTextsConfig } from './inputsConfig';
 import SelectInput from '../../../../reusable-ui/SelectInput';
+import FormInputs from './FormInputs';
 
 
 
@@ -15,28 +16,7 @@ const Form = React.forwardRef(({ onSubmit, onChange, product, children, onFocus,
     return (
         <FormStyled onSubmit={onSubmit}>
             <ImagePreview imageSource={product.imageSource} title={product.title} handleChange={onChange} />
-            <div className='input-fields'>
-                {inputTexts.map((input) => (
-                    <TextInput
-                        {...input}
-                        key={input.id}
-                        onChange={onChange}
-                        version="minimalist"
-                        ref={ref && input.name === "title" ? ref : null}
-                        onFocus={onFocus}
-                        onBlur={onBlur}
-                    />
-                ))}
-                {inputSelects.map((inputSelect) => (
-                    <SelectInput
-                        key={inputSelect.id}
-                        {...inputSelect}
-                        onChange={onChange}
-                        onFocus={onFocus}
-                        onBlur={onBlur}
-                    />
-                ))}
-            </div>
+            <FormInputs onFocus={onFocus} onBlur={onBlur} onChange={onChange} product={product} ref={ref} />
             <div className='submit'>{children}</div>
         </FormStyled>
     )
@@ -53,23 +33,6 @@ row-gap: 8px;
 height: 100%;
 width: 70%;
 
-.input-fields {
-    grid-area: 1 / 2 / 4 / 5;
-
-    display: grid;
-    grid-template-rows: repeat(3, 1fr);
-    grid-template-columns: repeat(3, 1fr);
-    row-gap: 8px;
-    column-gap: 8px;
-
-    .title {
-        grid-area: 1 / 1 / 2 / 4;
-    }
-    .imageSource{
-        grid-area: 2 / 1 / 3 / 4;
-    }
-
-}
 
 .submit {
     grid-area: 4 / 2 / 5 / 5; 
---
+++ src/component/pages/order/admin/admin-panel/FormInputs.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { styled } from 'styled-components';
+import { getInputSelectConfig, getInputTextsConfig } from './inputsConfig';
+import TextInput from '../../../../reusable-ui/TextInput.jsx'
+import SelectInput from '../../../../reusable-ui/SelectInput';
+
+
+const FormInputs = React.forwardRef(({ onFocus, onBlur, product, onChange }, ref) => {
+    const inputTexts = getInputTextsConfig(product)
+    const inputSelects = getInputSelectConfig(product)
+
+    return (
+        <FormInputsStyled className='input-fields'>
+            {inputTexts.map((input) => (
+                <TextInput
+                    {...input}
+                    key={input.id}
+                    onChange={onChange}
+                    version="minimalist"
+                    ref={ref && input.name === "title" ? ref : null}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                />
+            ))}
+            {inputSelects.map((inputSelect) => (
+                <SelectInput
+                    key={inputSelect.id}
+                    {...inputSelect}
+                    onChange={onChange}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                />
+            ))}
+        </FormInputsStyled>
+    )
+});
+
+export default FormInputs;
+
+const FormInputsStyled = styled.div`
+    grid-area: 1 / 2 / 4 / 5;
+    display: grid;
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+    row-gap: 8px;
+    column-gap: 8px;
+
+    .title {
+        grid-area: 1 / 1 / 2 / 4;
+    }
+    .imageSource{
+        grid-area: 2 / 1 / 3 / 4;
+    }
+
+`;
