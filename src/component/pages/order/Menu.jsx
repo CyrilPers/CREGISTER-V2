43448f661ceddd5cef901686c0f5d2be0c5a6144
Cyril <cyril.personne@outlook.fr>
2023-09-02 08:41:55 +0200

    RESPONSIVE

--- src/component/pages/order/Menu.jsx
+++ src/component/pages/order/Menu.jsx
@@ -92,11 +92,8 @@ const MenuStyled = styled.div`
   background: ${theme.colors.background_white};
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-
-
-  /* grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));  */
   grid-row-gap: 60px;
-  padding: 50px 50px 150px;
+  padding: 50px 50px 50px;
   justify-items: center;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   overflow-y: scroll;
@@ -129,11 +126,10 @@ const MenuStyled = styled.div`
     grid-row-gap: 0px;
     max-width: 100%;
     overflow-x: hidden;
-
-  padding: 5px 5px 5px;
+    padding: 0;
 
     .card-container {
-      height: 170px;
+      height: 150px;
     }
   }
 `;
