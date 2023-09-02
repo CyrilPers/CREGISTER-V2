433eb4acb6a485471a8dd08d64e7afa8e526492e
Cyril <cyril.personne@outlook.fr>
2023-09-02 22:14:21 +0200

    RESPONSIVE:

--- src/component/pages/order/Main.jsx
+++ src/component/pages/order/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { styled } from 'styled-components';
 import { theme } from '../../../theme';
 import LeftColumn from '../Main/left-column/LeftColumn';
