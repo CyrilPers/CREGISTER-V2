5634ae22afaecd302be3f46db18b4aed67ada360
Cyril <cyril.personne@outlook.fr>
2023-09-07 15:46:29 +0200

    🐛

--- src/component/pages/order/Main.jsx
+++ src/component/pages/order/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { styled } from 'styled-components';
-import { theme } from '../../../theme';
+import { theme } from '../../../theme/index';
 import LeftColumn from '../Main/left-column/LeftColumn';
 import MainRightSide from '../Main/main-right-side/MainRightSide';
 
