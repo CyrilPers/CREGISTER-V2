6ee7a3dd300bcb8c948d39ff735685ff6f2a532d
Cyril <cyril.personne@outlook.fr>
2023-09-16 20:40:54 +0200

    🚧 Chose customer in basket

--- src/component/pages/Main/left-column/basket/CustomerBlock.jsx
+++ src/component/pages/Main/left-column/basket/CustomerBlock.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import styled from 'styled-components';
+import { theme } from '../../../../../theme';
+import { TiDelete } from 'react-icons/ti'
 
 export default function CustomerBlock({ customer }) {
     return (
-        <>
-            <h1>{customer.name}</h1>
-        </>
+        <CustomerBlockStyled>
+
+            <div className='customer'>{customer.surname} {customer.name}</div>
+            <div className='buttons'>
+                <TiDelete className='button' />
+            </div>
+        </CustomerBlockStyled>
+
     )
 }
+
+const CustomerBlockStyled = styled.div`
+    display: inline-block;
+
+    .customer {
+        width: 100px;
+        height: 50px;
+        font-size: ${theme.fonts.size.P2};
+        font-family: ${theme.fonts.family.stylish};
+        border: 2px solid ${theme.colors.primary};
+        border-radius: ${theme.borderRadius.round};
+        color: black;
+        background-color: ${theme.colors.greyLight}
+    }
+    .buttons {
+        height:50px;
+        width:auto;
+        .button {
+            height:50px;
+            width:auto;
+        }
+    }
+`;
